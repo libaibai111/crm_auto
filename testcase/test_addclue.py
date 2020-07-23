24bb94fd82c73b155dfa3693f735119f6e3724a8
kitty <535889071@qq.com>
2020-07-23 22:43:30 +0800

    修改test_addclue.py添加线索case

--- testcase/test_addclue.py
+++ testcase/test_addclue.py
@@ -4,18 +4,16 @@ from page.addclue_page import AddCluePage
 from page.login_page import LoginPage
 from testcase.base_test import BaseTest
 
-
 class ClueTestCase(BaseTest):
     def test_clue(self):
         login = LoginPage(self.driver)  # 实例化loginPage类
         u_name = 'admin'
         password = 'admin123'
+        contact = 'du先生'
         login.login(u_name, password)  # 调用login page的login方法
         addclue = AddCluePage(self.driver)  # 调用AddCluePage
         time.sleep(3)
-        text = addclue.ele_addclue()  # 调用AddCluePage的ele_addclue方法
-
-
+        text = addclue.ele_addclue(contact)  # 调用AddCluePage的ele_addclue方法
 
         self.assertIn("线索添加成功", text)
 if __name__ == '__main__':
