4754d4d787a094fc44e93cea587f97861af70eb7
kitty <535889071@qq.com>
2020-07-23 22:42:04 +0800

    修改clue_page.py线索page

--- page/clue_page.py
+++ page/clue_page.py
@@ -23,9 +23,9 @@ class CluePage(BasePage):
         self.find_element(self.locator_newcreate).click()  # 点击新建线索
         print("点击新建线索")
 
-    def ele_contact(self):
+    def ele_contact(self,contact):
         '''输入联系人名称'''
-        self.find_element(self.locator_contact).send_keys('李先生')  # 输入联系人名称
+        self.find_element(self.locator_contact).send_keys(contact)  # 输入联系人名称
         print("输入联系人名称")
 
     def ele_save(self):
@@ -38,9 +38,9 @@ class CluePage(BasePage):
         self.find_element(self.locator_switch).click()  # 点击转换
         print("点击转换")
 
-    def ele_customername(self):
+    def ele_customername(self,customer):
         '''输入客户名称'''
-        self.find_element(self.locator_customername).send_keys('李白白')  # 输入客户名称
+        self.find_element(self.locator_customername).send_keys(customer)  # 输入客户名称
         print("输入客户名称")
 
     def ele_scroll(self):
@@ -55,13 +55,14 @@ class CluePage(BasePage):
         self.find_element(self.locator_switchsave).click()  # 点击保存
         print("点击保存")
 
-    def ele_clue(self):
+    def ele_clue(self,contact,customer):
+        '''添加线索转换为客户'''
         self.ele_clickclue()
         self.ele_newcreate()
-        self.ele_contact()
+        self.ele_contact(contact)
         self.ele_save()
         self.ele_switch()
-        self.ele_customername()
+        self.ele_customername(customer)
         self.ele_scroll()
         self.ele_switchsave()
         text = self.assert_result(self.locator_assert)
