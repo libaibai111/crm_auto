61eafc23875a89b48ea5dca764f95506adfeb54d
kitty <535889071@qq.com>
2020-07-23 22:43:53 +0800

    修改test_clue.py线索case

--- testcase/test_clue.py
+++ testcase/test_clue.py
@@ -8,11 +8,13 @@ class ClueTestCase(BaseTest):
         login = LoginPage(self.driver)  # 实例化loginPage类
         u_name = 'admin'
         password = 'admin123'
+        contact = '李先生'
+        customer = '李白白'
         login.login(u_name, password)  # 调用login page的login方法
 
         clue = CluePage(self.driver)  # 调用clue_page
 
-        text = clue.ele_clue()  # 调用clue_page的ele_clue方法
+        text = clue.ele_clue(contact,customer)  # 调用clue_page的ele_clue方法
 
         self.assertIn("添加客户成功", text)
 if __name__ == '__main__':
